efdc57bdda8d8e0bd35fcde68842a70bd899b490
20182115147a <truongnguyenquang1611@gmail.com>
2025-03-11 13:23:09 +0700

    change cros

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import { fileURLToPath } from 'url'
 import path, {dirname } from 'path'
-
+const corsOptions = {
+  origin: 'https://e-web-frontend.vercel.app/'
+}
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 mongoose.connect('mongodb+srv://Abc123456:Abc123456@cluster0.mgsmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use('/public/uploads', express.static(path.join(__dirname, 'public/uploads')))
 routes(app)
