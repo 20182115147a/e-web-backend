c6cbedfdabfc5d53ce002fd9f60e0365155f17b1
20182115147a <truongnguyenquang1611@gmail.com>
2025-03-12 15:32:03 +0700

    fix cros

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 import { fileURLToPath } from 'url'
 import path, {dirname } from 'path'
 const corsOptions = {
-  origin: 'http://localhost:5173'
+  origin: 'https://e-web-frontend.vercel.app'
 }
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
